d4302422b053978b4dcd226e8067aac5fc101b71
delfo <delformatik@gmail.com>
2023-06-05 20:36:14 +0300

    lesson1

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main{
+
+public static void main(String[] args)
+{
+    /**
+     * Написать программу, которая запросит пользователя ввести <Имя> в консоли.
+     * Получит введенную строку и выведет в консоль сообщение “Привет, <Имя>!”
+     *
+     */
+//     Scanner Scanner = new Scanner(System.in);
+//     String line = Scanner.next();
+
+// System.out.println("Привет "+line);
+}
+}
